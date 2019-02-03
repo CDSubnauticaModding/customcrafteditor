93152fb0cbcb853ed03490074e9b1b83c86da6ee
doriannaujokat <35139623+doriannaujokat@users.noreply.github.com>
2019-02-03 23:47:16 +0100

    ---

--- editor/dev-1.0.0.js
+++ editor/dev-1.0.0.js
@@ -4,26 +4,35 @@ window.onload = function() {
       document.getElementById("add-ingredient-confirm").onclick = new_ingredient;
       document.getElementById("add-linked-confirm").onclick = new_linked;
       document.getElementById("add-unlock-confirm").onclick = new_unlock;
-      document.getElementById("change-to-added").onclick = function() {change_mode(0)};
-      document.getElementById("change-to-modified").onclick = function() {change_mode(1)};
-      document.getElementById("change-to-alias").onclick = function() {change_mode(2)};
+      document.getElementById("editor-input-toggle").onclick = toggle_input;
+      document.getElementById("load-input").onclick = loadInput;
+      document.getElementById("change-to-nothing").onclick = function() {change_mode(0)};
+      document.getElementById("change-to-added").onclick = function() {change_mode(1)};
+      document.getElementById("change-to-modified").onclick = function() {change_mode(2)};
+      document.getElementById("change-to-alias").onclick = function() {change_mode(3)};
+      document.getElementById("change-to-customtab").onclick = function() {change_mode(4)};
       change_mode(0);
+      update_input_visibility();
       outputData();
 }
 
 function outputData() {
       var outp = document.getElementById("code-output");
       var strings = "";
-      if (outputMode === 0) {
+      if (outputMode === 1) {
             var __o = new OutputData(_addedRecipes,1);
             strings = toCString(__o);
       }
-      if (outputMode === 1) {
+      if (outputMode === 2) {
             var __o = new OutputData(_modifiedRecipes,2);
             strings = toCString(__o);
       }
-      if (outputMode === 2) {
+      if (outputMode === 3) {
             var __o = new OutputData(_aliasRecipes,3);
+            strings = toCString(__o);
+      }
+      if (outputMode === 4) {
+            var __o = new OutputData(_customCraftingTabs,4);
             strings = toCString(__o);
       }
       outp.innerHTML = strings;
@@ -51,13 +60,13 @@ function new_recipe() {
       var t_displayname = document.getElementById("add-display").value;
       var t_tooltip = document.getElementById("add-tooltip").value;
       var _recipe = new Recipe(t_id,t_amount,temp_Ingredients,temp_Linked,t_forceunlock,t_forceunlockdefault,temp_Unlock,t_path,t_displayname,t_tooltip);
-      if (outputMode === 0) {
+      if (outputMode === 1) {
             _addedRecipes.push(_recipe);
       }
-      if (outputMode === 1) {
+      if (outputMode === 2) {
             _modifiedRecipes.push(_recipe);
       }
-      if (outputMode === 2) {
+      if (outputMode === 3) {
             _aliasRecipes.push(_recipe);
       }
       temp_Ingredients = [];
@@ -97,16 +106,74 @@ function new_unlock() {
 function change_mode(m) {
       outputMode = m;
       var el = document.getElementById("output-mode");
+      var el_recipes = document.getElementById("editor-recipe");
+      var el_customtabs = document.getElementById("editor-customtab");
       if (outputMode === 0) {
+            el.innerHTML = "Output - Nothing selected";
+            el_recipes.hidden = "hidden";
+            el_customtabs.hidden = "hidden";
+      } else if (outputMode === 1) {
             el.innerHTML = "Output - AddedRecipe";
-      }
-      if (outputMode === 1) {
+            el_recipes.hidden = "";
+            el_customtabs.hidden = "hidden";
+      } else if (outputMode === 2) {
             el.innerHTML = "Output - ModifiedRecipe";
-      }
-      if (outputMode === 2) {
+            el_recipes.hidden = "";
+            el_customtabs.hidden = "hidden";
+      } else if (outputMode === 3) {
             el.innerHTML = "Output - AliasRecipe";
+            el_recipes.hidden = "";
+            el_customtabs.hidden = "hidden";
+      } else if (outputMode === 4) {
+            el.innerHTML = "Output - CustomCraftingTab";
+            el_recipes.hidden = "hidden";
+            el_customtabs.hidden = "";
       }
       outputData();
+}
+
+function loadInput() {
+      var elIn = document.getElementById("code-input");
+      var stringIn = elIn.value;
+      elIn.value = "";
+      var parsed = parseString(stringIn);
+      _addedRecipes = [];
+      _modifiedRecipes = [];
+      _aliasRecipes = [];
+      _customCraftingTabs = [];
+      if (parsed.mode === 1) {
+            _addedRecipes = parsed.data;
+            change_mode(1);
+      } else if (parsed.mode === 2) {
+            _modifiedRecipes = parsed.data;
+            change_mode(2);
+      } else if (parsed.mode === 3) {
+            _aliasRecipes = parsed.data;
+            change_mode(3);
+      } else if (parsed.mode === 4) {
+            _customCraftingTabs = parsed.data;
+            change_mode(4);
+      }
+      change_input_visibility(false);
+      outputData();
+}
+
+function toggle_input() {
+      change_input_visibility(!showInput);
+}
+
+function change_input_visibility(visible) {
+      showInput = visible;
+      update_input_visibility();
+}
+
+function update_input_visibility() {
+      var el = document.getElementById("editor-input");
+      if (showInput) {
+            el.hidden = "";
+      } else {
+            el.hidden = "hidden";
+      }
 }
 
 // VARS
@@ -114,9 +181,11 @@ function change_mode(m) {
 var _addedRecipes = [];
 var _modifiedRecipes = [];
 var _aliasRecipes = [];
+var _customCraftingTabs = [];
 
 var temp_Ingredients = [];
 var temp_Linked = [];
 var temp_Unlock = [];
 
 var outputMode = 0;
+var showInput = false;
